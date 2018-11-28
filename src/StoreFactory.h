f8854d2c2481fcd69bedd0a174f7dc15f98245dc
knonomura <katsuhiko.nonomura@griddb.org>
2018-11-28 15:52:15 +0900

    change version message

--- src/StoreFactory.h
+++ src/StoreFactory.h
@@ -16,7 +16,7 @@
 
 #ifndef _STORE_FACTORY_H_
 #define _STORE_FACTORY_H_
-#define CLIENT_VERSION "GridDB Node.JS Client 0.7"
+#define CLIENT_VERSION "GridDB Node.JS Client 0.8"
 
 #include "gridstore.h"
 #include "Store.h"
