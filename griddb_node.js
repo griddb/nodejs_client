a99024d6f682c88407b1a16dfff469f7cdfd56d2
knonomura <katsuhiko.nonomura@griddb.org>
2018-12-27 20:28:19 +0900

    modify resolve()/reject()

--- griddb_node.js
+++ griddb_node.js
@@ -10,11 +10,10 @@ class StoreFactory {
     getStore(obj) {
         return new Store(this.factory.getStore(obj));
     }
-  
+
     static getInstance() {
         return new StoreFactory(griddb.StoreFactory_.getInstance());
     }
-    
     getVersion() {
         return this.factory.getVersion();
     }
@@ -24,7 +23,7 @@ class Store {
     constructor(store) {
         this.store = store;
     }
-  
+
     // Async functions
     putContainer(info, modifiable = false) {
         var this_ = this;
@@ -34,7 +33,7 @@ class Store {
                     var container = this_.store.putContainer(info, modifiable);
                     resolve(new Container(container));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
@@ -48,12 +47,12 @@ class Store {
                     var container = this_.store.getContainer(name);
                     resolve(new Container(container));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     dropContainer(name) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
@@ -61,12 +60,12 @@ class Store {
                 try {
                     resolve(this_.store.dropContainer(name));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     getContainerInfo(name) {
         var this_= this;
         return new Promise(function(resolve, reject) {
@@ -74,12 +73,12 @@ class Store {
                 try {
                     resolve(this_.store.getContainerInfo(name));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     // Sync functions
 
 };
@@ -88,26 +87,25 @@ class Container {
     constructor(container) {
         this.container = container;
     }
-    
+
     get type() {
         return this.container.type;
     }
-  
+
     // Async functions
     put(arr) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function(){
                 try {
-                    this_.container.put(arr);
-                    resolve("Put row successfully");
+                    resolve(this_.container.put(arr));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-  
+
     get(key) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
@@ -116,135 +114,127 @@ class Container {
                     var row = this_.container.get(key);
                     resolve(row);
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     remove(key) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try{
-                    this_.container.remove(key);
-                    resolve("Remove row successfully");
+                    resolve(this_.container.remove(key));
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     createIndex(columnName, indexType = griddb.GS_INDEX_FLAG_DEFAULT , name = null) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
                     if (name == null) {
-                        this_.container.createIndex(columnName, indexType);
+                        resolve(this_.container.createIndex(columnName, indexType));
                     } else {
-                        this_.container.createIndex(columnName, indexType, name);
+                        resolve(this_.container.createIndex(columnName, indexType, name));
                     }
-                    resolve("Create index successfully");
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     dropIndex(columnName, indexType = griddb.GS_INDEX_FLAG_DEFAULT , name = null) {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
                     if (name == null) {
-                        this_.container.dropIndex(columnName, indexType);
+                        resolve(this_.container.dropIndex(columnName, indexType));
                     } else {
-                        this_.container.dropIndex(columnName, indexType, name);
+                        resolve(this_.container.dropIndex(columnName, indexType, name));
                     }
-                    resolve("Drop index successfully");
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     flush() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.container.flush();
-                    resolve("Flush successfully");
+                    resolve(this_.container.flush());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     abort() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.container.abort();
-                    resolve("Abort successfully");
+                    resolve(this_.container.abort());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     commit() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.container.commit();
-                    resolve("Commit successfully");
+                    resolve(this_.container.commit());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     close() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.container.close();
-                    resolve("Close successfully");
+                    resolve(this_.container.close());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     // Sync functions
     query(strQuery) {
         try {
             return new Query(this.container.query(strQuery));
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
-    
+
     setAutoCommit(commitFlg) {
         try {
             this.container.setAutoCommit(commitFlg);
         } catch(err) {
-            throw(err);
+            reject(convertToGSException(err));
         }
     }
-
 };
 
 class Query {
@@ -260,105 +250,99 @@ class Query {
                   var rowSet = this_.query.fetch(false);
                   resolve(new RowSet(rowSet));
               } catch(err) {
-                  reject(err);
+                  reject(convertToGSException(err));
               }
           }, 0);
         });
     }
-    
+
     close() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.query.close();
-                    resolve("Close successfully");
+                    resolve(this_.query.close());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     // Sync functions
     getRowSet() {
         try {
             return new RowSet(this.query.getRowSet());
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
-    
+
     setFetchOptions(option) {
         try {
             this.query.setFetchOptions(option);
         } catch (err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
-
 };
 
 class RowSet {
     constructor(rowSet) {
         this.rowSet = rowSet;
     }
-    
+
     get size() {
         return this.rowSet.size;
     }
-    
+
     get type() {
         return this.rowSet.type;
     }
-    
+
     // Async functions
     close() {
         var this_ = this;
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
                 try {
-                    this_.rowSet.close();
-                    resolve("Close successfully");
+                    resolve(this_.rowSet.close());
                 } catch(err) {
-                    reject(err);
+                    reject(convertToGSException(err));
                 }
             }, 0);
         });
     }
-    
+
     // Sync functions
     next() {
         try {
             return this.rowSet.next();
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
-    
+
     hasNext() {
         try {
             return this.rowSet.hasNext();
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
-
 }
 
 class AggregationResult {
     constructor(aggResult) {
         this.aggResult = aggResult;
     }
-    
-    // Async functions
-    
+
     // Sync function
     get(type) {
         try {
             return this.aggResult.get(type);
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
 }
@@ -367,26 +351,52 @@ class QueryAnalysisEntry {
     constructor(queryAnalysis) {
         this.queryAnalysis = queryAnalysis;
     }
-    
+
     // Sync function
     get() {
         try {
             return this.queryAnalysis.get();
         } catch(err) {
-            throw(err);
+            throw(convertToGSException(err));
         }
     }
 }
 
-var x = {
-        StoreFactory: StoreFactory,
-        Store: Store,
-        Container: Container,
-        Query: Query,
-        AggregationResult: AggregationResult,
-        QueryAnalysis: QueryAnalysis,
-        RowSet: RowSet,
+class GSException{
+    constructor(exception) {
+        this.exception = exception;
+    }
+    // Sync function
+    isTimeout() {
+        return this.exception.isTimeout();
+    }
+    getErrorStackSize() {
+        return this.exception.getErrorStackSize();
+    }
+    getErrorCode(stackIndex) {
+        return this.exception.getErrorCode(stackIndex);
+    }
+    getMessage(stackIndex) {
+        return this.exception.getMessage(stackIndex);
+    }
+    getLocation() {
+        return this.exception.getLocation(stackIndex);
+    }
+    what() {
+        return this.exception.what();
+    }
 }
+
+function convertToGSException(exception) {
+    if (exception.constructor.name == "_exports_GSException") {
+        return new GSException(exception);
+    }
+    return exception;
+}
+
+var x = {
+        StoreFactory: StoreFactory
+};
 
 for (var key in x) {
     griddb[key] = x[key];
