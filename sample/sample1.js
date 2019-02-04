5f917cd24efe3b9a9d83f67c687dfceebc734377
mochizk <shohei.mochizuki@toshiba.co.jp>
2019-02-04 14:00:15 +0900

    Improve sample code

--- sample/sample1.js
+++ sample/sample1.js
@@ -1,52 +1,51 @@
 var griddb = require('griddb_node');
-var fs     = require('fs');
+var fs = require('fs');
 
 var factory = griddb.StoreFactory.getInstance();
 var store = factory.getStore({
-                        "host": process.argv[2], 
-                        "port": parseInt(process.argv[3]),
-                        "clusterName": process.argv[4], 
-                        "username": process.argv[5], 
-                        "password": process.argv[6]});
+	"host": process.argv[2],
+	"port": parseInt(process.argv[3]),
+	"clusterName": process.argv[4],
+	"username": process.argv[5],
+	"password": process.argv[6]
+});
 
-var conInfo = new griddb.ContainerInfo({'name' : "col01",
-    'columnInfoList' : [
-            ["name", griddb.Type.STRING],
-            ["status", griddb.Type.BOOL],
-            ["count", griddb.Type.LONG],
-            ["lob", griddb.Type.BLOB]
-        ], 
-        'type' : griddb.ContainerType.COLLECTION, 'rowKey' : true});
+var conInfo = new griddb.ContainerInfo({
+	'name': "col01",
+	'columnInfoList': [
+		["name", griddb.Type.STRING],
+		["status", griddb.Type.BOOL],
+		["count", griddb.Type.LONG],
+		["lob", griddb.Type.BLOB]
+	],
+	'type': griddb.ContainerType.COLLECTION, 'rowKey': true
+});
 
-var col2;
+var container;
 store.putContainer(conInfo, false)
-	.then(col => {
-	 	col2 = col;
-	 	col.createIndex({'columnName' : 'count', 'indexType': griddb.IndexType.DEFAULT});
-		return col;
-  	})
-	.then(col => {
-		col.setAutoCommit(false);
-		col.put(["name01", false, 1, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])]);
-		return col;
+	.then(cont => {
+		container = cont;
+		return container.createIndex({ 'columnName': 'count', 'indexType': griddb.IndexType.DEFAULT });
 	})
-	.then(col => {
-		col.commit();
-		return col;
+	.then(() => {
+		container.setAutoCommit(false);
+		return container.put(["name01", false, 1, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])]);
 	})
-	.then(col => {
-		query = col.query("select *");
+	.then(() => {
+		return container.commit();
+	})
+	.then(() => {
+		query = container.query("select *")
 		return query.fetch();
 	})
 	.then(rs => {
 		while (rs.hasNext()) {
 			console.log(rs.next());
 		}
-		col2.commit();
+		return container.commit();
 	})
 	.catch(err => {
-		if (err.constructor.name == "_exports_GSException") {
-			console.log(err.what());
+		if (err.constructor.name == "GSException") {
 			for (var i = 0; i < err.getErrorStackSize(); i++) {
 				console.log("[", i, "]");
 				console.log(err.getErrorCode(i));
