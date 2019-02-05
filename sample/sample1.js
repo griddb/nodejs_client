34149ea12ff06ffba938aa5ea1c3e513d8d5311a
mochizk <shohei.mochizuki@toshiba.co.jp>
2019-02-05 09:12:14 +0900

    Replace tab with space and run formatter again

--- sample/sample1.js
+++ sample/sample1.js
@@ -3,55 +3,55 @@ var fs = require('fs');
 
 var factory = griddb.StoreFactory.getInstance();
 var store = factory.getStore({
-	"host": process.argv[2],
-	"port": parseInt(process.argv[3]),
-	"clusterName": process.argv[4],
-	"username": process.argv[5],
-	"password": process.argv[6]
+    "host": process.argv[2],
+    "port": parseInt(process.argv[3]),
+    "clusterName": process.argv[4],
+    "username": process.argv[5],
+    "password": process.argv[6]
 });
 
 var conInfo = new griddb.ContainerInfo({
-	'name': "col01",
-	'columnInfoList': [
-		["name", griddb.Type.STRING],
-		["status", griddb.Type.BOOL],
-		["count", griddb.Type.LONG],
-		["lob", griddb.Type.BLOB]
-	],
-	'type': griddb.ContainerType.COLLECTION, 'rowKey': true
+    'name': "col01",
+    'columnInfoList': [
+        ["name", griddb.Type.STRING],
+        ["status", griddb.Type.BOOL],
+        ["count", griddb.Type.LONG],
+        ["lob", griddb.Type.BLOB]
+    ],
+    'type': griddb.ContainerType.COLLECTION, 'rowKey': true
 });
 
 var container;
 store.putContainer(conInfo, false)
-	.then(cont => {
-		container = cont;
-		return container.createIndex({ 'columnName': 'count', 'indexType': griddb.IndexType.DEFAULT });
-	})
-	.then(() => {
-		container.setAutoCommit(false);
-		return container.put(["name01", false, 1, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])]);
-	})
-	.then(() => {
-		return container.commit();
-	})
-	.then(() => {
-		query = container.query("select *")
-		return query.fetch();
-	})
-	.then(rs => {
-		while (rs.hasNext()) {
-			console.log(rs.next());
-		}
-		return container.commit();
-	})
-	.catch(err => {
-		if (err.constructor.name == "GSException") {
-			for (var i = 0; i < err.getErrorStackSize(); i++) {
-				console.log("[", i, "]");
-				console.log(err.getErrorCode(i));
-				console.log(err.getMessage(i));
-			}
-		} else {
-			console.log(err);
-		}
-	});
+    .then(cont => {
+        container = cont;
+        return container.createIndex({ 'columnName': 'count', 'indexType': griddb.IndexType.DEFAULT });
+    })
+    .then(() => {
+        container.setAutoCommit(false);
+        return container.put(["name01", false, 1, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])]);
+    })
+    .then(() => {
+        return container.commit();
+    })
+    .then(() => {
+        query = container.query("select *")
+        return query.fetch();
+    })
+    .then(rs => {
+        while (rs.hasNext()) {
+            console.log(rs.next());
+        }
+        return container.commit();
+    })
+    .catch(err => {
+        if (err.constructor.name == "GSException") {
+            for (var i = 0; i < err.getErrorStackSize(); i++) {
+                console.log("[", i, "]");
+                console.log(err.getErrorCode(i));
+                console.log(err.getMessage(i));
+            }
+        } else {
+            console.log(err);
+        }
+    });
--- sample/sample2.js
+++ sample/sample2.js
@@ -1,50 +1,49 @@
 var griddb = require('griddb_node');
-var fs     = require('fs');
+var fs = require('fs');
 
 var factory = griddb.StoreFactory.getInstance();
 var store = factory.getStore({
-        "host": process.argv[2],
-        "port": parseInt(process.argv[3]),
-        "clusterName": process.argv[4],
-        "username": process.argv[5],
-        "password": process.argv[6]
-    });
+    "host": process.argv[2],
+    "port": parseInt(process.argv[3]),
+    "clusterName": process.argv[4],
+    "username": process.argv[5],
+    "password": process.argv[6]
+});
 
-var conInfo = new griddb.ContainerInfo(
-    {
-        'name': "point01",
-        'columnInfoList': [
-            ["timestamp", griddb.Type.TIMESTAMP],
-            ["active", griddb.Type.BOOL],
-            ["voltage", griddb.Type.DOUBLE]
-        ],
-        'type': griddb.ContainerType.TIME_SERIES, 'rowKey': true
-    });
+var conInfo = new griddb.ContainerInfo({
+    'name': "point01",
+    'columnInfoList': [
+        ["timestamp", griddb.Type.TIMESTAMP],
+        ["active", griddb.Type.BOOL],
+        ["voltage", griddb.Type.DOUBLE]
+    ],
+    'type': griddb.ContainerType.TIME_SERIES, 'rowKey': true
+});
 
 var time_series;
 store.putContainer(conInfo, false)
-	.then(ts => {
+    .then(ts => {
         time_series = ts;
         return ts.put([new Date(), false, 100]);
     })
-	.then(() => {
+    .then(() => {
         query = time_series.query("select * where timestamp > TIMESTAMPADD(HOUR, NOW(), -6)");
         return query.fetch();
     })
-	.then(rowset => {
+    .then(rowset => {
         while (rowset.hasNext()) {
             var row = rowset.next();
             console.log("Time =", row[0], "Active =", row[1].toString(), "Voltage =", row[2]);
         }
     })
     .catch(err => {
-		if (err.constructor.name == "GSException") {
-			for (var i = 0; i < err.getErrorStackSize(); i++) {
-				console.log("[", i, "]");
-				console.log(err.getErrorCode(i));
-				console.log(err.getMessage(i));
-			}
-		} else {
-			console.log(err);
-		}
+        if (err.constructor.name == "GSException") {
+            for (var i = 0; i < err.getErrorStackSize(); i++) {
+                console.log("[", i, "]");
+                console.log(err.getErrorCode(i));
+                console.log(err.getMessage(i));
+            }
+        } else {
+            console.log(err);
+        }
     });
